47246672299007ed94bcb0d28376244d1e3b1188
RZ <c2067748@newcastle.ac.uk>
2024-02-20 06:58:19 +0000

    Added defintions to config.h

---
+++ src/config.h
@@ -0,0 +1,3 @@
+#define CURRENT_DIR "." 
+#define PREV_DIR ".." 
+#define DIR_FORMAT "Name: %s\t Type: %s\t Date <created>: %s\t Data <modified>: %s\t"
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <dirent.h>
 
+#include "config.h"
+
 int main(int argc, char **argv) {
 
     if (argc <= 1) {
@@ -10,10 +12,11 @@ int main(int argc, char **argv) {
 
     DIR *d;
     struct dirent *dir;
-    d = opendir(".");
+    d = opendir(CURRENT_DIR);
     if (d) {
         while ((dir = readdir(d)) != NULL) {
           printf("%s\n", dir->d_name);
+          printf(DIR_FORMAT, dir->d_name, dir->d_name, dir->d_name, dir->d_name);
         }
         closedir(d);
     }
